1c47ba77a6cef685d0da93ab8cf2400260f20ff4
liqingzhao <li.qingz@northeastern.edu>
2024-04-22 10:53:16 -0700

    finished provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Authenticated, GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import {dataProvider, liveProvider} from "./providers";
+import {dataProvider, liveProvider, authProvider} from "./providers";
+import {Home, ForgotPassword, Register, Login} from "./pages";
+
 
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Layout from "antd/es/layout/layout";
 
 
 
@@ -28,7 +32,7 @@ function App() {
                 liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
-                // authProvider={}
+                authProvider={authProvider}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
@@ -38,7 +42,22 @@ function App() {
                 }}
               >
                 <Routes>
-                  <Route index element={<WelcomePage />} />
+                  
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
+                  <Route
+                    element={<Authenticated 
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }>
+                    <Route index element={<Home />} />
+                  </Route>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CurrentUser from './current-user'
+
+export const Header = () => {
+    return (
+        <div>
+            <CurrentUser/>
+        </div>
+    )
+}
+
